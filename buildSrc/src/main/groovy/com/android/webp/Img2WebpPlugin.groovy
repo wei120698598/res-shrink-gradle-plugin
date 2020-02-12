7e30def54ecd0eba28a87e4d1beadcaa418aed59
weishuxin <weishuxin@maoyan.com>
2020-02-12 15:39:24 +0800

    v1.0.4-release

--- buildSrc/src/main/groovy/com/android/webp/Img2WebpPlugin.groovy
+++ buildSrc/src/main/groovy/com/android/webp/Img2WebpPlugin.groovy
@@ -14,12 +14,16 @@ class Img2WebpPlugin implements Plugin<Project> {
     private Project project
     private String buildTypeName
     private File logFile
+    private WebpOptions webpOptions = new WebpOptions()
+    private int compressSize = 0
+    private int count = 0
 
     @Override
     void apply(Project project) {
         this.project = project
         def hasApp = project.plugins.withType(AppPlugin)
         def currTime
+        webpOptions = project.extensions.create("webpOptions", WebpOptions)
 
         def variants = hasApp ? project.android.applicationVariants : project.android.libraryVariants
         project.afterEvaluate {
@@ -28,6 +32,7 @@ class Img2WebpPlugin implements Plugin<Project> {
                 def dx = project.tasks.findByName("package${variant.name.capitalize()}")
                 def webpConvertPlugin = "img2webpPlugin${variant.name.capitalize()}"
                 project.tasks.create(webpConvertPlugin) {
+
                     Utils.BUILD_DIR = "${project.buildDir}"
                     def resPath = "${Utils.BUILD_DIR}/intermediates/processed_res/debug/processDebugResources/out"
                     doFirst {
@@ -40,6 +45,8 @@ class Img2WebpPlugin implements Plugin<Project> {
                             logFile.parentFile.mkdirs()
                         }
                         logFile.createNewFile()
+                        logFile.append("AppVersionName:${project.android.defaultConfig.versionName}\n")
+                        logFile.append("Compress quality ${webpOptions.quality}, delete image regex: ${webpOptions.delImgRegex}\n")
                         "rm -rf $resPath/${Utils.RES_APK_NAME}".execute().waitFor()
 
                         Utils.copyWebpLib()
@@ -53,10 +60,15 @@ class Img2WebpPlugin implements Plugin<Project> {
                             //删除原res_apk
                             resApk.delete()
                             //img2webp
-                            def count = img2webp(new File(zipDir, "res"))
+                            img2webp(new File(zipDir, "res"))
                             Utils.zip(resApk.absolutePath, zipDir.listFiles())
-                            Utils.log "${(System.currentTimeMillis() - currTime) / 1000.0f}s to process $count files."
+                            def msg = "${(System.currentTimeMillis() - currTime) / 1000.0f}s to process $count files. Compress size:${String.format("%.2f", compressSize / 1024.0f)}kb"
+                            logFile.append(msg)
+                            Utils.log msg
                         }
+                    }
+                    project.tasks.findByName(webpConvertPlugin).onlyIf {
+                        webpOptions.enable
                     }
                     project.tasks.findByName(webpConvertPlugin).dependsOn dx.taskDependencies.getDependencies(dx)
                     dx.dependsOn project.tasks.findByName(webpConvertPlugin)
@@ -66,8 +78,8 @@ class Img2WebpPlugin implements Plugin<Project> {
 
     }
 
-    private int img2webp(File dir) {
-        int count = 0
+    private void img2webp(File dir) {
+        def isDel = webpOptions.delImgRegex != null && webpOptions.delImgRegex.trim() != ""
         dir.eachDirMatch(~/drawable[a-z0-9-]*/) { resDir ->
             def resCharIndex = resDir.absolutePath.indexOf("/res/")
             resDir.eachFile { resFile ->
@@ -82,21 +94,33 @@ class Img2WebpPlugin implements Plugin<Project> {
                         isInWhiteList = true
                     }
                 }
-                if (!isInWhiteList && !name.contains(".9") && (name.endsWith(".jpg") || name.endsWith(".png") || name.endsWith(".gif"))) {
+                def originSize = resFile.length()
+                if (isDel && resFile.name.matches(webpOptions.delImgRegex)) {
+                    "rm ${resFile.absolutePath}".execute().waitFor()
+                    logFile.append("${resFile.absolutePath.substring(resCharIndex)}: del ${originSize}->0\n")
+                    compressSize += originSize
+                } else if (!isInWhiteList && !name.contains(".9") && (name.endsWith(".jpg") || name.endsWith(".png") || name.endsWith(".gif"))) {
                     def executeProgram = "cwebp"
                     if (name.endsWith(".gif")) {
                         executeProgram = "gif2webp"
                     }
-                    def picName = name.split('\\.')[0]
-                    if ("${Utils.WEBP_LIB_BIN_PATH}/bin/${executeProgram}  -q 75 -m 6 ${resFile} -o ${resDir}/${picName}.webp".execute().waitFor() != 0) {
+//                    def picName = name.split('\\.')[0]
+                    if ("${Utils.WEBP_LIB_BIN_PATH}/bin/${executeProgram}  -q ${webpOptions.quality} -m 6 ${resFile} -o ${resFile}".execute().waitFor() != 0) {
                         throw new RuntimeException("${resFile} error ")
                     }
-                    "rm ${resFile}".execute().waitFor()
-                    logFile.append("${resFile.absolutePath.substring(resCharIndex)}-> ${resDir.absolutePath.substring(resCharIndex)}/${picName}.webp\n")
+                    def cSize = resFile.length()
+                    def compSize = originSize - cSize
+                    logFile.append("${resFile.absolutePath.substring(resCharIndex)}: cmp ${originSize}->${cSize} size: ${compSize} \n")
                     count++
+                    compressSize += compSize
                 }
             }
         }
-        return count
+    }
+
+    static class WebpOptions {
+        int quality = 75
+        boolean enable = true
+        String delImgRegex
     }
 }
